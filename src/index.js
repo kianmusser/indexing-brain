263136b29a91b5c1ade22363f8a4e81453983ce1
Kian Musser <kian@kianmusser.com>
2024-02-05 17:11:43 -0600

    implement the most basic search possible

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 import express from "express";
-import path from 'node:path';
+import path from "node:path";
 import { fileURLToPath } from 'node:url';
 import fs from "node:fs/promises";
 import { z } from "zod";
+import child_process from "node:child_process";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // adapted from https://stackoverflow.com/questions/196972/convert-string-to-title-case-with-javascript/64910248
 const titleCase = (str) => str.toLowerCase().replace(/\b\w/g, s => s.toUpperCase());
+
+
+// from https://stackoverflow.com/questions/10623798/how-do-i-read-the-contents-of-a-node-js-stream-into-a-string-variable
+function streamToString(stream) {
+  const chunks = [];
+  return new Promise((resolve, reject) => {
+    stream.on('data', (chunk) => chunks.push(Buffer.from(chunk)));
+    stream.on('error', (err) => reject(err));
+    stream.on('end', () => resolve(Buffer.concat(chunks).toString('utf8')));
+  })
+}
 
 
 class Server {
@@ -52,6 +64,20 @@ class Server {
     res.send(locationsNoFolder);
   }
 
+  async search(query, files, count) {
+    const proc = child_process.spawn("/usr/bin/rg", ["--crlf", "-H", "-m", count, query, ...files]);
+    const output = await streamToString(proc.stdout);
+    return output.split("\n").map((line) => {
+      const pieces = line.split(":");
+      const fileName = pieces.shift();
+      const name = pieces.join(":").trim();
+
+      const place = fileName.split("/").slice(-1)[0].slice(0, -5).toUpperCase();
+      return { name, place };
+
+    });
+  }
+
   async searchHandler(req, res) {
     const locations = await this.getLocations();
     const locAbbrs = locations.map((l) => l.abbr);
@@ -75,7 +101,12 @@ class Server {
     const curFilePath = path.join(curLoc.folder, curFileName);
     console.log(params, curFilePath);
 
-    res.send("Search");
+    let results = [];
+    const output = await this.search(params.query, [curFilePath], 100);
+    results.push(...output);
+    console.log("output", output);
+
+    res.send(results);
   }
 
   log(...args) {
